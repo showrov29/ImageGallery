24d4e4ef7c0a27f0780aad014be0933733841a0f
Showrov Islam <63032286+showrov29@users.noreply.github.com>
2023-11-01 01:24:27 +0600

    feat: #2 #4 #1 sorting , swapping and layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+
 import Gallery from "./components/Gallery";
 
 function App() {
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,9 +1,63 @@
-import React from "react";
-
+import React, { useState } from "react";
+import data from "../data/Data";
 export default function Gallery() {
+	const [images, setImages] = useState(data);
+
+	const [draggedImage, setDraggedImage] = useState(null);
+
+	const handleDragStart = (e, id) => {
+		setDraggedImage(id);
+	};
+	const handleDrop = (e, id) => {
+		if (draggedImage !== null && draggedImage !== id) {
+			const newImages = images.slice(); // Create a copy of the images array
+			const draggedImageIndex = newImages.findIndex(
+				(image) => image.id === draggedImage
+			);
+			const targetImageIndex = newImages.findIndex((image) => image.id === id);
+
+			if (draggedImageIndex !== -1 && targetImageIndex !== -1) {
+				// Swap the positions of the dragged image and the target image
+				[newImages[draggedImageIndex], newImages[targetImageIndex]] = [
+					newImages[targetImageIndex],
+					newImages[draggedImageIndex],
+				];
+
+				setImages(newImages);
+			}
+		}
+
+		setDraggedImage(null);
+	};
+
 	return (
-		<div>
-			<h1>Gallery</h1>
+		<div className="bg-gray-100 min-h-screen p-4">
+			<h1 className="text-2xl mb-4">Gallery</h1>
+			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+				{images.map((image, index) => (
+					<div
+						key={image.id}
+						className={`group p-2 relative bg-white rounded-lg border transition-opacity hover:opacity-30  duration-300 cursor-pointer ${
+							index == 0 ? "border-4 border-blue-500 col-span-2 row-span-2" : ""
+						} ${image.isSelected ? "bg-gray-300" : ""}`}
+						onDragStart={(e) => handleDragStart(e, image.id)}
+						onDrop={(e) => handleDrop(e, image.id)}
+						onDragOver={(e) => e.preventDefault()}>
+						<input
+							type="checkbox"
+							className="absolute  h-5 w-10 top-5 left-5 z-100 opacity-0 group-hover:opacity-100"
+						/>
+
+						<img
+							src={image.src}
+							alt={`Image ${image.id}`}
+							className={`max-w-full mx-auto transition-all duration-300 ease-in-out transform ${
+								image.id === draggedImage ? "scale-110" : "scale-90"
+							}`}
+						/>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 }
