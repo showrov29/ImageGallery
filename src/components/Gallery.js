c75d27e43d557c84af264673a9ac3c9ee1531944
Showrov Islam <63032286+showrov29@users.noreply.github.com>
2023-11-02 01:08:21 +0600

    add image section

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -21,26 +21,39 @@ export default function Gallery() {
 	const handleDragStart = (e, id) => {
 		setDraggedImage(id);
 	};
+	/**
+	 * Handle the drop event for a draggable image.
+	 *
+	 * @param {Event} e - The drop event object.
+	 * @param {string} id - The unique identifier of the target image where the dragged image is dropped.
+	 */
 	const handleDrop = (e, id) => {
 		if (draggedImage !== null && draggedImage !== id) {
-			const newImages = images.slice(); // Create a copy of the images array
+			// Create a copy of the images array
+			const newImages = images.slice();
+
+			// Find the index of the dragged image in the newImages array
 			const draggedImageIndex = newImages.findIndex(
 				(image) => image.id === draggedImage
 			);
+
+			// Find the index of the target image in the newImages array
 			const targetImageIndex = newImages.findIndex((image) => image.id === id);
 
 			if (draggedImageIndex !== -1 && targetImageIndex !== -1) {
-				// Swap the positions of the dragged image and the target image
+				// Swap the positions of the dragged image and the target image in the newImages array
 				[newImages[draggedImageIndex], newImages[targetImageIndex]] = [
 					newImages[targetImageIndex],
 					newImages[draggedImageIndex],
 				];
 
+				// Update the state with the newImages array
 				setImages(newImages);
+
+				// Optionally, you can set a property on the target image like this:
+				// newImages[targetImageIndex].isDropable = true;
 			}
 		}
-
-		setDraggedImage(null);
 	};
 
 	const countSelectedCheckboxes = Object.values(checkboxValues).filter(
@@ -81,7 +94,9 @@ export default function Gallery() {
 							index == 0
 								? "border-4 border-orange-300 col-span-2 row-span-2"
 								: ""
-						} ${image.isSelected ? "bg-gray-300" : ""}`}
+						} ${image.isSelected ? "bg-gray-300" : ""}} ${
+							image.id === draggedImage ? "image-dragged" : ""
+						}'`}
 						onDragStart={(e) => handleDragStart(e, image.id)}
 						onDrop={(e) => handleDrop(e, image.id)}
 						onDragOver={(e) => e.preventDefault()}>
@@ -96,12 +111,28 @@ export default function Gallery() {
 						<img
 							src={image.src}
 							alt={`Image ${image.id}`}
-							className={`max-w-full mx-auto transition-all duration-300 ease-in-out transform ${
-								image.id === draggedImage ? "scale-110" : "scale-90"
-							}`}
+							className={`max-w-full mx-auto transition-all duration-300 ease-in-out transform`}
 						/>
 					</div>
 				))}
+				<label
+					className={`h-full w-full rounded-lg flex flex-col space-y-5 items-center justify-center border-2 border-dashed border-slate-400 transition-all transform hover:scale-105 focus:scale-105 active:scale-95`}>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth="1.5"
+						stroke="currentColor"
+						className="w-6 h-6">
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+						/>
+					</svg>
+					<input type="file" className="hidden" accept="image/*" />
+					<h1>Add Image</h1>
+				</label>
 			</div>
 		</div>
 	);
